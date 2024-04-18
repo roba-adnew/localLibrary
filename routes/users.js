111beba7bd74cfd6fbf8b7c043d1f6240fa1185b
roba <10408936+roba-adnew@users.noreply.github.com>
2024-04-18 16:38:09 -0400

    Practicing routes

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/cool', function(req, res) {
+  res.render('cool', { title: "gas", emphasizer: "damn" })
+})
+
 module.exports = router;
+
+
